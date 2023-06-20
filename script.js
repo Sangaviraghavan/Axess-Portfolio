97a58be4d6141e8107e4fbc41d9e561bf4716b78
sara <sangaviraghavan2002@gmail.com>
2023-06-20 12:06:06 +0530

    New2 changes

--- script.js
+++ script.js
@@ -9,6 +9,23 @@ $(document).ready(function () {
 
     })
   })
+
+    document.getElementById("myForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent  form submission
+
+      // Get form values
+      var name = document.getElementById("name").value;
+      var email = document.getElementById("email").value;
+      var message = document.getElementById("message").value;
+
+      // Do something with the form data
+      console.log("Name:", name);
+      console.log("Email:", email);
+      console.log("Message:", message);
+
+      // Reset form
+      document.getElementById("myForm").reset();
+    });
     // smooth scrolling
 
 //     $('a[href*="#"]').on('click',function (e) {
